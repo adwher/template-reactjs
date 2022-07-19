e905e18461e7525e139d5591d012555e4e6591a8
Andres Celis <adwher@icloud.com>
2022-07-19 10:05:37 -0500

    docs: upgrade documentation of `import.meta.env`

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,20 +1,33 @@
-// STYLES
+// styles
 
 declare module "*.css"
 declare module "*.sass"
 declare module "*.scss"
 
-// VITEJS
+// vitejs
 
 declare interface ImportMeta {
-    /** Vite exposes [env variables](https://vitejs.dev/guide/env-and-mode.html#env-variables-and-modes) on the special `import.meta.env` object. Some built-in variables are available in all cases */
+    /**
+     * Vite exposes [env variables](https://vitejs.dev/guide/env-and-mode.html#env-variables-and-modes) on the special `import.meta.env` object. Some built-in variables are available in all cases like `DEV` or `MODE` and also can type some variants in the `types.d.ts` file.
+     *
+     * @example
+     * ```ts
+     * declare interface ImportMeta {
+     *  /// Other declarations...
+     *
+     *  env: {
+     *      readonly VITE_APP_HELLO_WORLD: string
+     *  }
+     * }
+     * ```
+     */
     env: {
-        MODE: string
-        BASE_URL: string
-        PROD: boolean
-        DEV: boolean
+        readonly MODE: string
+        readonly BASE_URL: string
+        readonly PROD: boolean
+        readonly DEV: boolean
 
-        [key: string]: any
+        readonly [key: string]: any
     }
 
     /** Vite supports [importing multiple modules](https://vitejs.dev/guide/features.html#glob-import) from the file system via the special `import.meta.glob` function */
